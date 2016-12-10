f1a7559f8bf26ac57a723d2d2d7bbfe82d7198f9
fyksi <fyksi@DESKTOP-RMUOB2U>
2016-12-10 13:53:48 +0300

    macro

--- asm_ind/asm_ind/main.asm
+++ asm_ind/asm_ind/main.asm
@@ -4,10 +4,41 @@
 
 .stack
 
+andm macro x, y, z
+	 movapd z, x
+	 andpd z, y
+endm
+
+orm macro x, y, z
+	 movapd z, x
+	 orpd z, y
+endm
+
+xorm macro x, y, res
+	 movaps res, x
+	 xorps res, y
+endm
+
+nandm macro x, y, z
+	 movapd z, x
+	 andnpd z, y
+endm
+
+notm macro x, y
+	movapd y, x
+	andnpd y, y
+endm
+
+ornm macro x, y, z
+	movapd z, x
+	orpd z, y
+	andnpd
+endm
+
 .data
 
 .code
 _main proc
-
+	xor eax, eax
 _main endp
 end _main
